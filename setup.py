c8028a35edd4d01681594dcae8ac5819f2547ac5
Borna Bešić <besic.borna@gmail.com>
2019-11-08 01:26:17 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name="ex-machina",
+    author="Borna Bešić",
+    description="Access the state of a running program via socket-based REPL",
+    packages=find_packages(),
+)
